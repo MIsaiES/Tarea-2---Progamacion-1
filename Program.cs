cbc31aa091ddd0d7fec4b13b6ee4fc8998829403
MIsaiES <isaiesquivel9382@gmail.com>
2024-02-16 19:52:33 -0600

    Operaciones, finalizadas

--- Program.cs
+++ Program.cs
@@ -2,10 +2,11 @@
 
 	int n1=0, n2=0, x1=0, x2=0;
 	int c1 = 0;
-	string txt;
+	double r, d1, d2;
+	float f1, f2;
 do{	
 		Console.WriteLine("¿Que desea hacer?");
-		Console.WriteLine("1.Conversion de Tipos \n2.Operaciones\n3.Manejo de Texto\n4.Calculos y Conversiones\n0.Salir");
+		Console.WriteLine("1.Conversion de Tipos \n2.Operaciones\n0.Salir");
 		x1=Convert.ToUInt16(Console.ReadLine());
 
 	switch (x1)
@@ -41,43 +42,69 @@ do{
 			{
 				case 1:
 					n1= (int)n1;
-					break;
+                    Console.WriteLine("Conversion Exitosa, el valor es :" + n1);
+                    break;
 				case 2:
-					float f1=Convert.ToSingle(n1);
-					break;
+					float f=Convert.ToSingle(n1);
+                    Console.WriteLine("Conversion Exitosa, el valor es :" + f);
+                    break;
 				case 3:
 					n1= (int)Convert.ToDecimal(n1);
-					break;
+                    Console.WriteLine("Conversion Exitosa, el valor es :" + n1);
+                    break;
                 case 4:
                     n1 = Convert.ToChar(value: n1);
+                    Console.WriteLine("Conversion Exitosa, el valor es :" + n1);
                     break;
                 default:
 					break;
 			}
-			Console.WriteLine("Conversion Exitosa, el valor es :"+n1);
 			break;
 		case 2:
 			Console.WriteLine("Ingrese la operacion que desea realizar");
 			Console.WriteLine("1.Suma de Enteros\n2.Resta de Decimales\n3.Multiplicacion de Flotantes");
-			Console.WriteLine("4.Division de Entros\n5.Modulo de Enteros");
+			Console.WriteLine("4.Division y Modulo de Entros");
 			x2 = Convert.ToUInt16(Console.ReadLine());
 			switch (x2)
 			{
 				case 1:
-					/////////////////////////////////////////////////////
+					Console.WriteLine("Ingrese un numero");
+					n1 = Convert.ToInt16(Console.ReadLine());
+					Console.WriteLine("Ingrese un segundo numero");
+					n2 = Convert.ToInt16(Console.ReadLine());
+					r = n1 + n2;
+					Console.WriteLine("La suma de los enteros es de: "+r);
 					break;
+				case 2:
+                    Console.WriteLine("Ingrese un numero");
+                    d1 = (double)Convert.ToDecimal(Console.ReadLine());
+                    Console.WriteLine("Ingrese un segundo numero");
+                    d2 = (double)Convert.ToDecimal(Console.ReadLine());
+                    r = d1 - d2;
+                    Console.WriteLine("La resta de los decimales  es de: " + r);
+                    break;
+				case 3:
+                    Console.WriteLine("Ingrese un numero");
+                    f1 = float.Parse(Console.ReadLine());
+                    Console.WriteLine("Ingrese un segundo numero");
+                    f2 = float.Parse(Console.ReadLine());
+                    r = f1 * f2;
+                    Console.WriteLine("La multiplicacion de los flotantes es de: " + r);
+                    break;
+                case 4:
+                    Console.WriteLine("Ingrese un numero");
+                    n1 = Convert.ToInt16(Console.ReadLine());
+                    Console.WriteLine("Ingrese un segundo numero");
+                    n2 = Convert.ToInt16(Console.ReadLine());
+                    r = n1 / n2;
+                    Console.WriteLine("La division de los enteros es de: " + r);
+                    r = n2 % n1;
+                    Console.WriteLine("El modulo de los enteros es de" + r);
+                    break;
 				default:
 					Console.WriteLine("Opcion INVALIDA");
 					break;
 			}
-			break;
-		case 3:
-			Console.WriteLine("Ingrese su cadena de texto");
-			txt = Console.ReadLine();
-			Console.WriteLine("");
-			break;
-		case 4:
-
 			break;
 		default:
 			Console.WriteLine("Ingrese una opcion VALIDA");
